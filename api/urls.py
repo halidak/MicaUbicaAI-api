e7cd5c7ae2960451a81ec7332632be5aa827a9d5
Halida Karišik <103694518+halidak@users.noreply.github.com>
2024-11-26 19:21:52 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from .views import your_view_function
 from .views import reset_game
 from .compViews import compVScomp, reset_gameComp
-from .pytnik.pytnikViews import agent_path
 
 
 urlpatterns = [
     path('stones', your_view_function, name='your_view_function'),
     path('comp', compVScomp, name="compVscomp"),
     path('resetC', reset_gameComp, name='reset_gameC'),
-    path('reset', reset_game, name='reset_game'),
-    path('get-path', agent_path, name='agent_path'),
+    path('reset', reset_game, name='reset_game')
 ]
